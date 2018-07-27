ebe5f2f056346055622b21c58b1a597a96f80d1f
Anthony <anthonyovalles@gmail.com>
2018-07-27 10:32:04 -0400

    Configuraciones para la comunicacion del API

---
+++ MedBack/Models/DataContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Web;
+
+namespace MedBack.Models
+{
+    public class DataContext : DbContext
+    {
+        public DataContext() : base("DefaultConnection")
+        {
+        }
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<OneToManyCascadeDeleteConvention>();
+        }
+
+        public DbSet<Product> Products { get; set; }
+    }
+
+}
